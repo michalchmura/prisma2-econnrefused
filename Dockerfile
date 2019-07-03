774f3def344f4822e98de8ce19d774499a5e90b6
Michal Chmura <michal.chmura@klarna.com>
2019-07-03 16:40:09 +0200

    🚀 Init

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM node:12.4.0
+
+ADD ./ /opt/app
+WORKDIR /opt/app
+
+USER root
+
+RUN rm -rf node_modules \
+  && npm install \
+  && chown -R node /opt/app \
+  && yarn postinstall
+
+USER node
+
+ENV HOME_DIR=/opt/app \
+  NODE_CLUSTERED=0 \
+  NODE_ENV=production \
+  NODE_HOT_RELOAD=0 \
+  PORT=8080
+
+EXPOSE 8001
+EXPOSE 8080
+EXPOSE 8081
+
+CMD ["npm", "run", "start"]
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { nexusPrismaPlugin } from '@generated/nexus-prisma';
+import Photon from '@generated/photon';
+import { makeSchema } from '@prisma/nexus';
+import { GraphQLServer } from 'graphql-yoga';
+import express, { Request, Response, RequestHandler } from 'express';
+import { join } from 'path';
+import { permissions } from './permissions';
+import * as allTypes from './resolvers';
+import { Context } from './types';
+
+const photon = new Photon({
+  debug: true,
+});
+
+const nexusPrisma = nexusPrismaPlugin({
+  photon: (ctx: Context) => ctx.photon,
+});
+
+const schema = makeSchema({
+  types: [allTypes, nexusPrisma],
+  outputs: {
+    typegen: join(__dirname, '../generated/nexus-typegen.ts'),
+    schema: join(__dirname, '/schema.graphql'),
+  },
+  typegenAutoConfig: {
+    sources: [
+      {
+        source: '@generated/photon',
+        alias: 'photon',
+      },
+      {
+        source: join(__dirname, 'types.ts'),
+        alias: 'ctx',
+      },
+    ],
+    contextType: 'ctx.Context',
+  },
+});
+
+const healthCheckServer = () => {
+  const server = express();
+
+  server.get('/ping', (_: Request, res: Response) => res.send('pong'));
+  server.listen(8081, () =>
+    console.log('Health Checks are running on port 8081')
+  );
+};
+
+const server = new GraphQLServer({
+  schema,
+  // middlewares: [permissions], // TODO: Fix after https://github.com/maticzav/graphql-shield/issues/361
+  context: request => {
+    return {
+      ...request,
+      photon,
+    };
+  },
+});
+
+server.start({ port: 8080 }, () =>
+  console.log(`🚀 Server ready at http://localhost:8080`)
+);
+
+healthCheckServer();
